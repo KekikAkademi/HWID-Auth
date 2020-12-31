933c4969ec1bacb8c24458dc999ef043b04c7cc8
Soud <57543084+Soud69@users.noreply.github.com>
2020-12-31 19:41:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
+# Importing
+
 import subprocess
 import time
 import request
 import os
-hw = subprocess.check_output('wmic csproduct get uuid').decode().split('\n')[1].strip()
-a = requests.get('url here')
- 
+
+hw = subprocess.check_output('wmic csproduct get uuid').decode().split('\n')[1].strip() # Grapping HWID
+
+a = requests.get('url here') # Making GET Request
+
 try:
-    if hw in a.text:
+    if hw in a.text: # Checking HWID In URL
         print('[+] HWID Signed In Success')
-        pass
+        pass # If HWID In URL Then Success
     else:
         print('[!] HWID Not Found')
         print(f'HWID: {hw}') 
         time.sleep(5)
-        os._exit()
+        os._exit() # If HWID Not In URL Then Fail + Close os
+
 except:
     print('[?] Failed to connect to database')
     time.sleep(5) 
-    os._exit() 
+    os._exit() # Check for Fail Connection
  
-os.system('title HWID Authentication')
+os.system('title HWID Authentication') # OS Title
  
-print('Hello, World!')
+print('Hello, World!') # Hello World :)
 input()
